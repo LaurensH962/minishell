43f5e2f10290fcb834dec14076f9175ab8a02cc3
Laurens Haas <lhaas@c1r4p1.hive.fi>
2025-04-08 11:40:36 +0300

    lexer refactoring

--- src/lexing/token.c
+++ src/lexing/token.c
@@ -42,47 +42,6 @@ void	add_token(t_token **head, t_token *new)
 }
 
 
-/* len = ft_strlen(token_value);
-if	((token_value[0] == '\'' && token_value[len - 1] == '\'')
-	|| (token_value[0] == '"' && token_value[len - 1] == '"'))
-{
-	cleaned_value = ft_strndup(token_value + 1, len - 2);
-	free(token_value);
-	token_value = cleaned_value;
-} */
-// size_t	len;
-// char	*cleaned_value;
-
-
-/* t_token	*lexer_next_token(t_lexer *lexer, t_token *temp_token, char quote_char,
-		char *token_value)
-{
-	while (lexer->input[lexer->pos])
-	{
-		if (!quote_char && ft_isspace(lexer->input[lexer->pos]))
-		{
-			lexer->pos++;
-			continue ;
-		}
-		if (!quote_char)
-		{
-			temp_token = create_delim_token(lexer);
-			if (temp_token)
-				return (temp_token);
-		}
-		token_value = ft_strdup("");
-		temp_token = inner_loop((int *)&lexer->pos, &quote_char, &token_value,
-				lexer);
-		if (temp_token)
-			return (temp_token);
-		if (quote_char)
-			return (free(token_value), new_token(TOKEN_ERROR,
-					"Unclosed quote"));
-		return (new_token(TOKEN_WORD, token_value));
-	}
-	return (new_token(TOKEN_EOF, NULL));
-} */
-
 t_token	*split_expanded_value(t_token *head, char *token_value)
 {
 	char	*saveptr;
@@ -112,48 +71,103 @@ t_token	*split_expanded_value(t_token *head, char *token_value)
 	return (head);
 }
 
-static t_token	*inner_loop(int *pos, char *quote_char, char **token_value,
-	t_lexer *lexer)
+// static t_token	*inner_loop(int *pos, char *quote_char, char **token_value,
+// 	t_lexer *lexer)
+// {
+// 	char *before;
+// 	char *after;
+
+// 	while (lexer->input[*pos])
+// 	{
+// 		if ((lexer->input[*pos] == '"' || lexer->input[*pos] == '\'')
+// 				&& (*quote_char == lexer->input[*pos] || *quote_char == 0))
+// 		{
+// 			if (*quote_char == lexer->input[*pos])
+// 			{
+// 				if (*token_value == NULL)
+// 					*token_value = ft_strdup("");
+// 				*quote_char = 0;
+// 			}
+// 			else if (*quote_char == 0)
+// 			{
+// 				*quote_char = lexer->input[*pos];
+// 				lexer->was_quoted = 1;
+// 			}
+// 			(*pos)++;
+// 			continue ;
+// 		}
+// 		else if (lexer->input[*pos] == '$' && *quote_char != '\''
+// 			&& lexer->hereflag != '<')
+// 		{
+// 			before = *token_value;
+// 			after = find_expanded_value(lexer, before);
+
+// 			if (after)
+// 				*token_value = after;
+// 			//*token_value = find_expanded_value(lexer, *token_value);
+// 			lexer->was_expanded = 1;
+// 			//printf("{%p}", *token_value);
+// 			//printf("----expanded token = %s ----\n", *token_value);
+// 			/* if (!*token_value)
+// 			{
+// 				//return (new_token(TOKEN_ERROR, "Variable expansion error"));
+// 				return (NULL);
+// 			} */
+// 			continue ;
+// 		}
+// 		else if (!*quote_char && (ft_isspace(lexer->input[*pos])
+// 				|| !is_delimiter(lexer->input[*pos])))
+// 			break ;
+// 		if (*token_value != NULL)
+// 			*token_value = ft_strncat(*token_value, &lexer->input[*pos], 1);
+// 		(*pos)++;
+// 	}
+// 	return (NULL);
+// }
+
+static void lexer_quotes(char *quote_char, t_lexer *lexer, int *pos, char **token_value)
+{
+	if (*quote_char == lexer->input[*pos])
+	{
+		if (*token_value == NULL)
+			*token_value = ft_strdup("");
+		*quote_char = 0;
+	}
+	else if (*quote_char == 0)
+	{
+		*quote_char = lexer->input[*pos];
+		lexer->was_quoted = 1;
+	}
+	(*pos)++;
+}
+
+static void lexer_expander(t_lexer *lexer, char **token_value)
 {
 	char *before;
 	char *after;
 
+	before = *token_value;
+	after = find_expanded_value(lexer, before);
+	if (after)
+		*token_value = after;
+	lexer->was_expanded = 1;
+}
+
+static t_token	*inner_loop(int *pos, char *quote_char, char **token_value,
+	t_lexer *lexer)
+{
 	while (lexer->input[*pos])
 	{
 		if ((lexer->input[*pos] == '"' || lexer->input[*pos] == '\'')
 				&& (*quote_char == lexer->input[*pos] || *quote_char == 0))
 		{
-			if (*quote_char == lexer->input[*pos])
-			{
-				if (*token_value == NULL)
-					*token_value = ft_strdup("");
-				*quote_char = 0;
-			}
-			else if (*quote_char == 0)
-			{
-				*quote_char = lexer->input[*pos];
-				lexer->was_quoted = 1;
-			}
-			(*pos)++;
+			lexer_quotes(quote_char, lexer, pos, token_value);
 			continue ;
 		}
 		else if (lexer->input[*pos] == '$' && *quote_char != '\''
 			&& lexer->hereflag != '<')
 		{
-			before = *token_value;
-			after = find_expanded_value(lexer, before);
-
-			if (after)
-				*token_value = after;
-			//*token_value = find_expanded_value(lexer, *token_value);
-			lexer->was_expanded = 1;
-			//printf("{%p}", *token_value);
-			//printf("----expanded token = %s ----\n", *token_value);
-			/* if (!*token_value)
-			{
-				//return (new_token(TOKEN_ERROR, "Variable expansion error"));
-				return (NULL);
-			} */
+			lexer_expander(lexer, token_value);
 			continue ;
 		}
 		else if (!*quote_char && (ft_isspace(lexer->input[*pos])
